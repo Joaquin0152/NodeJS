02fb52f101477047032a828d3d350b0e5a3d58df
Joaquin Maravillo <joaquin297545@gmail.com>
2021-04-10 17:54:05 -0500

    had to change the location of where the answeres are suppose to go

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
+//node modules
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 
-// create writeFile function using promises instead of a callback function
-const writeFileAsync = util.promisify(fs.writeFile);
+// Our readme info
+const writeFile = util.promisify(fs.writeFile);
 
 const promptUser = () => {
-  return inquirer.prompt([
-    {
+    return inquirer.prompt([
+      {
       type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+      name: 'title',
+      message: 'What is your project title?',
     },
     {
-      type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
+        type: 'input',
+        name: 'instructions',
+        message: 'Instructions to be followed?',
     },
     {
-      type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
+        type: 'input',
+        name: 'credits',
+        message: 'Do you have any credits?',
     },
     {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
+      type: 'list',
+      name: 'license',
+      message: 'What licensing am I using?',
+      choices: ['MIT','Apache', 'ISC']
     },
     {
       type: 'input',
@@ -34,42 +36,17 @@ const promptUser = () => {
     },
     {
       type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
+      name: 'email',
+      message: 'What is your email address',
     },
-  ]);
+]);
 };
-
-const generateHTML = (answers) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
-
-// Bonus using writeFileAsync as a promise
+//Writing a FileAsync as a promise
 const init = () => {
-  promptUser()
-    .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
-    .then(() => console.log('Successfully wrote to index.html'))
-    .catch((err) => console.error(err));
-};
-
-init();
+    promptUser()
+      .then((answers) => writeFile('README.md', generateHTML(answers)))
+      .then(() => console.log('Successfully wrote to README.md'))
+      .catch((err) => console.error(err));
+  };
+  // Initialize
+  init();
